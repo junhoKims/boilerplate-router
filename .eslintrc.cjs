25d10f627788e654408b21fa712ca7b5df549f8c
Junho Kim <40831445+junhoKims@users.noreply.github.com>
2023-10-18 21:59:35 +0900

    chore: init
    - eslint, typescript, emotion, css base

---
+++ .eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  env: { browser: true, node: true, es6: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['build', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'unused-imports'],
+  rules: {
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'off',
+    "unused-imports/no-unused-imports": "error",
+		"unused-imports/no-unused-vars": [
+			"warn",
+			{ "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+		],
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
+    'import/order': [
+      'error',
+      {
+        warnOnUnassignedImports: true,
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: '{.,..}/**/*.+(css|sass|less|scss|pcss|styl|svg)',
+            patternOptions: { dot: true, nocomment: true },
+            group: 'unknown',
+            position: 'after',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {}
+    }
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
+  build: {
+    outDir: "build",
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+});
