06069064eaf550cf4fcb268c496b6da002086e11
krak <kraksin12@gmail.com>
2024-01-31 23:30:57 +0900

    b

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,6 @@ export { default as Main } from "./Main";
 export { default as Settings } from "./Settings";
 export { default as NotFound } from "./NotFound";
 export * from "./Item";
+
+
+console.log('sksksksksk')
